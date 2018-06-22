83750100bc2583da1f4b664cfd41afcbb0605fa1
Radoslav Evgeniev <r.evgeniev.ivanov@gmail.com>
2018-06-22 15:00:28 +0300

    Summer MVC: Added Model class

---
+++ src/app/summer/api/Model.java
@@ -0,0 +1,25 @@
+package app.summer.api;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Model {
+    private Map<String, Object> attributes;
+
+    public Model() {
+        this.attributes = new HashMap<>();
+    }
+
+    public Map<String, Object> getAttributes() {
+        return Collections.unmodifiableMap(this.attributes);
+    }
+
+    public void addAttributes(String name, Object value) {
+        this.attributes.put(name, value);
+    }
+
+    public void removeAttribute(String name) {
+        this.attributes.remove(name);
+    }
+}
